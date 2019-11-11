e655e039ce1004d7a67442049f55653fde9ddb2b
Kaichun Wu <44331917+kaichunwu@users.noreply.github.com>
2019-11-11 01:43:14 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # java is required
 if ! type "java" > /dev/null;
 then
-    echo "Please install java and try again."
+ 	echo "Please install java and try again."
 	echo "Press ctrl+c to exit or wait for 5 seconds"
 	sleep 5
 else
